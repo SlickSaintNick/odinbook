8ced197836f202653d2e13b66548d98ea6524f01
SlickSaintNick <144679503+SlickSaintNick@users.noreply.github.com>
2024-04-24 07:20:43 +0930

    Update Navbar, add new and edit to Profiles controller.

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,2 +1,34 @@
 class ProfilesController < ApplicationController
+  def new
+    @profile = Profile.new
+  end
+
+  def create
+    @profile = Profile.new(profile_params)
+
+    if @profile.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @profile = Profile.find(params[:id])
+  end
+
+  def update
+    @profile = Profile.find(params[:id])
+    if @profile.update(article_params)
+      redirect_to root_path
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def profile_params
+    params.require(:profile).permit(:name, :bio, :role)
+  end
 end
--- app/models/profile.rb
+++ app/models/profile.rb
@@ -3,5 +3,9 @@ class Profile < ApplicationRecord
     user_profile: 0,
     admin_profile: 1
   }
+
+  validates :name, presence: true, length: { maximum: 100 }
+  validates :bio, length: { maximum: 1000 }
+
   belongs_to :user
 end
